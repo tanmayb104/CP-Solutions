aa603c97d29c6eba37aa26d4030053144404fdbe
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-12-17 20:44:54 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1454D | Number into Sequence/99450870.py
@@ -0,0 +1,104 @@
+import os
+import sys
+from io import BytesIO, IOBase
+# region fastio
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+ 
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline()
+ 
+# ------------------------------
+ 
+def RL(): return map(int, sys.stdin.readline().split())
+def RLL(): return list(map(int, sys.stdin.readline().split()))
+def N(): return int(input())
+def print_list(l):
+    print(' '.join(map(str,l)))
+# sys.setrecursionlimit(300000)
+# from heapq import *
+# from collections import deque as dq
+# from math import ceil,floor,sqrt,pow
+# import bisect as bs
+from collections import Counter
+# from collections import defaultdict as dc 
+import math
+ 
+def primeFactors(n): 
+    l=[]
+    # Print the number of two's that divide n 
+    while n % 2 == 0: 
+        l.append(2)
+        n = n / 2
+          
+    # n must be odd at this point 
+    # so a skip of 2 ( i = i + 2) can be used 
+    for i in range(3,int(math.sqrt(n))+1,2): 
+          
+        # while i divides n , print i ad divide n 
+        while n % i== 0: 
+            l.append(i)
+            n = n / i 
+    if n > 2: 
+        l.append(n)
+    return l
+ 
+for i in range(N()):
+    n=N()
+    l=primeFactors(n) 
+    c=Counter(l)
+    ma=0
+    j=0
+    for i in set(l):
+        if(c[i]>ma):
+            ma=c[i]
+            j=i
+    ans=[j]*(ma-1)
+    try:
+        ans.append(n//(j**(ma-1)))
+    except:
+        ans=[n]
+    print(len(ans))
+    print(*ans)
