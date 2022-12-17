f7e40212685f30fcf3244c7b9dedd8d8c95aab46
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-12-17 20:48:46 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1358A | Park Lighting/81488387.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    # n=int(input())
+    # l=[int(x) for x in input().split()]
+    #n, k = map(int, input().split())
+    n, m = map(int, input().split())
+    print((n * m + 1) // 2)
