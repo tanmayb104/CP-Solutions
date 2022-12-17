7ad1bb6e5cc9df977f5bc9282b4c4e97aa9c43cf
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-12-17 21:05:59 +0530

    Committing files

---
+++ CodeChef/PYTH 3/COVDSMPL/34377888.py
@@ -0,0 +1,40 @@
+for _ in range(int(input())):
+    n,p=map(int,input().split())
+    l=[]
+    for i in range(n):
+        l1=[]
+        for j in range(n):
+            l1.append(0)
+        l.append(l1)
+    i=1
+    print(1,1,1,n,n)
+    total=int(input())
+    if(total==0):
+        print(2)
+        for i in range(n):
+            print(*l[i])
+    else:
+        while(i<=n):
+            if(total==0):
+                break
+            else:
+                print(1,i,1,i,n)
+                total_c=int(input())
+                total-=total_c
+                j=1
+                while(j<=n-1):
+                    if(total_c==0):
+                        break
+                    print(1,i,j,i,j)
+                    if(int(input())==1):
+                        l[i-1][j-1]=1
+                        total_c-=1
+                    j+=1
+                if(total_c):
+                    l[i-1][j-1]=1
+            i+=1
+        print(2)
+        for i in range(n):
+            print(*l[i])
+    if(int(input())==-1):
+        break
