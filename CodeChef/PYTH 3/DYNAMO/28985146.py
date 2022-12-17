ea973c179128327fa75eed22cbe33d70025ecf57
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-12-17 21:09:22 +0530

    Committing files

---
+++ CodeChef/PYTH 3/DYNAMO/28985146.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for z in range(int(input())):
+    p=int(input())
+    q=(10**p)
+    a=int(input())
+    s=q*2+a
+    print(s)
+    b=int(input())
+    print(q-b)
+    d=int(input())
+    print(q-d)
+    if(int(input())==-1):
+        break
