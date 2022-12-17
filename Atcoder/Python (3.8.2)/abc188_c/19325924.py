d622915849bfd8a112cf3e023a7ab67915b8c571
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-12-17 20:37:42 +0530

    Committing files

---
+++ Atcoder/Python (3.8.2)/abc188_c/19325924.py
@@ -0,0 +1,97 @@
+# cook your dish here
+import os
+import sys
+from io import BytesIO, IOBase
+# region fastio
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+ 
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline()
+ 
+# ------------------------------
+ 
+def RL(): return map(int, sys.stdin.readline().split())
+def RLL(): return list(map(int, sys.stdin.readline().split()))
+def N(): return int(input())
+def print_list(l):
+    print(' '.join(map(str,l)))
+# sys.setrecursionlimit(300000)
+# from heapq import *
+from collections import deque as dq
+# from math import ceil,floor,sqrt,pow
+# import bisect as bs
+from collections import Counter
+# from collections import defaultdict as dc 
+import math
+
+n,c1=RLL()
+d={}
+l=[]
+for i in range(n):
+    a,b,c=RL()
+    if(a in d):
+        d[a]+=c
+    else:
+        d[a]=c
+    if(b+1 in d):
+        d[b+1]-=c
+    else:
+        d[b+1]=-c
+    l.append(a)
+    l.append(b+1)
+l=list(set(l))
+l.sort()
+c=0
+ans=0
+for i in range(len(l)-1):
+    c+=d[l[i]]
+    if(c<c1):
+        ans+=c*(l[i+1]-l[i])
+    else:
+        ans+=c1*(l[i+1]-l[i])
+print(ans)
+
+
