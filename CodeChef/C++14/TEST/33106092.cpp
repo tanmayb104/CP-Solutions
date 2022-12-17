c38b63f0f2b35096d018cb7535521b158a04d7d9
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-12-17 21:02:32 +0530

    Committing files

---
+++ CodeChef/C++14/TEST/33106092.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,i;
+	while(1){
+	    cin>>a;
+	    if(a==42){
+	        break;
+	    }
+	    else{
+	        cout<<a<<"\n";
+	    }
+	}
+	return 0;
+}
